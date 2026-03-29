20db088496b9242d417dbfa83fcafe101e2ba666
krishnaji020210 <krishnaji020210@gmail.com>
2026-03-29 08:24:32 +0530

    Update play.py

--- Spy/utils/inline/play.py
+++ Spy/utils/inline/play.py
@@ -70,8 +70,8 @@ def stream_markup_timer(_, chat_id, played, dur):
     InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}")
         ],
         [
-            InlineKeyboardButton(text="ʀᴇᴘʟᴀʏ[↻]", callback_data=f"ADMIN Replay|{chat_id}"),style=ButtonStyle.DANGER
-            InlineKeyboardButton(text="ᴄʟᴏsᴇ[✗]", callback_data="close",style=ButtonStyle.SUCCESS
+            InlineKeyboardButton(text="ʀᴇᴘʟᴀʏ[↻]", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="ᴄʟᴏsᴇ[✗]", callback_data="close",
             )
         ],
     ]
@@ -81,11 +81,11 @@ def stream_markup_timer(_, chat_id, played, dur):
 def stream_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),style=ButtonStyle.PRIMARY
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),style=ButtonStyle.SUCCESS
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),style=ButtonStyle.SUCCESS
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),style=ButtonStyle.DANGER
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),style=ButtonStyle.DEFAULT
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],style=ButtonStyle.DANGER
     ]
