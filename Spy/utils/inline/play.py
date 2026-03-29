a499b87be753fa6f071c75a4c8a650e4b2978246
krishnaji020210 <krishnaji020210@gmail.com>
2026-03-29 08:20:47 +0530

    Update play.py

--- Spy/utils/inline/play.py
+++ Spy/utils/inline/play.py
@@ -64,10 +64,10 @@ def stream_markup_timer(_, chat_id, played, dur):
             )
         ],
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),style=ButtonStyle.SUCCESS
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),style=ButtonStyle.PRIMARY
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),style=ButtonStyle.DEFAULT
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),style=ButtonStyle.DEFAULT
+    InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+    InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+    InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+    InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}")
         ],
         [
             InlineKeyboardButton(text="ʀᴇᴘʟᴀʏ[↻]", callback_data=f"ADMIN Replay|{chat_id}"),style=ButtonStyle.DANGER
